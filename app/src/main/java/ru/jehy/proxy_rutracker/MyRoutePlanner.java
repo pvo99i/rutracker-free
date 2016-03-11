8b5f3025397c54b15dc42005e70ac98a8134c160
Jehy <fate@jehy.ru>
2016-03-11 19:11:43 +0300

    everything is working... but the topic search

---
+++ app/src/main/java/ru/jehy/proxy_rutracker/MyRoutePlanner.java
@@ -0,0 +1,22 @@
+package ru.jehy.proxy_rutracker;
+
+import org.apache.http.HttpException;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.conn.routing.HttpRoute;
+import org.apache.http.conn.routing.HttpRoutePlanner;
+import org.apache.http.conn.routing.RouteInfo;
+import org.apache.http.protocol.HttpContext;
+
+/**
+ * Created by Bond on 2016-03-11.
+ */
+public class MyRoutePlanner implements HttpRoutePlanner {
+    @Override
+    public HttpRoute determineRoute(HttpHost target, HttpRequest request,
+                                    HttpContext context) throws HttpException {
+        return new HttpRoute(target, null
+                , new HttpHost("proxy.googlezip.net", 443, "https")
+                , true, RouteInfo.TunnelType.PLAIN, RouteInfo.LayerType.PLAIN); //Note: true
+    }
+}
